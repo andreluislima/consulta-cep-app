503ae34095f85cfa411ace977289f8d8e65a2cba
andreluislima <andrelimag89@gmail.com>
2026-03-07 11:16:44 -0300

    feat: cria projetos (frontend e backend)

---
+++ backend/register-cep-api/src/test/java/com/register/cep/api/RegisterCepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.register.cep.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
