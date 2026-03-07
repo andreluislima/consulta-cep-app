e8271e7f69334cf4aeaf608b5601a7775f45f7a1
andreluislima <andrelimag89@gmail.com>
2026-03-07 14:58:31 -0300

    chore(security): configurar permissoes de acesso para ambiente de desenvolvimento

--- backend/register-cep-api/src/main/java/com/register/cep/api/dto/UsuarioRequestDTO.java
+++ backend/register-cep-api/src/main/java/com/register/cep/api/dto/UsuarioRequestDTO.java
@@ -1,5 +1,8 @@
 package com.register.cep.api.dto;
 
-public record UsuarioRequestDTO(String cep, String nome, String cpf) {
+public record UsuarioRequestDTO(
+		String cep, 
+		String nome, 
+		String cpf) {
 
 }
