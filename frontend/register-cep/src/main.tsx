d3ee5756962dea18dcf3b7d7bfc9ed561f4610aa
andreluislima <andrelimag89@gmail.com>
2026-03-10 20:12:08 -0300

    chore(mai): configura react query.

--- frontend/register-cep/src/main.tsx
+++ frontend/register-cep/src/main.tsx
@@ -3,11 +3,18 @@ import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 30 } },
+});
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </QueryClientProvider>
   </BrowserRouter>,
 );
